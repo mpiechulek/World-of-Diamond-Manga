620e3fd987f3a7a8ceb385ab7e7fc2c931a56c84
MP <47081376+mpiechulek@users.noreply.github.com>
2021-01-06 10:52:24 +0100

    added abour and comic modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutLayoutComponent } from './layout/about/about-layout.component';
+import { ComicLayoutComponent } from './layout/comic/comic-layout.component';
 import { HomeLayoutComponent } from './layout/home/home-layout.component';
 
 const routes: Routes = [
@@ -14,6 +16,22 @@ const routes: Routes = [
     loadChildren: () =>
       import('./modules/home/home.module').then(
         m => m.HomeModule
+      )
+  },
+  {
+    path: 'about',
+    component: AboutLayoutComponent,
+    loadChildren: () =>
+      import('./modules/about/about.module').then(
+        m => m.AboutModule
+      )
+  },
+  {
+    path: 'comic',
+    component: ComicLayoutComponent,
+    loadChildren: () =>
+      import('./modules/comic/comic.module').then(
+        m => m.ComicModule
       )
   }
 ];
