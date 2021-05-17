3485d4b2fb8aa2c8be6ddbe05c8a4a6ce8c267ba
MP <47081376+mpiechulek@users.noreply.github.com>
2021-05-17 17:57:14 +0200

    added 3rd chapter

--- src/app/core/services/chapter-pages.service.ts
+++ src/app/core/services/chapter-pages.service.ts
@@ -9,7 +9,7 @@ export class ChapterPagesService {
   comicArr: ChapterModel[] = [
     { name: 'chapter-1', display: 'Chapter 1', length: 46, filename: "ch-001" },
     { name: 'chapter-2', display: 'Chapter 2', length: 18, filename: "ch-002" },
-    // { name: 'chapter-3', display: 'Chapter 3', length: 22, filename: "ch-003" },
+    { name: 'chapter-3', display: 'Chapter 3', length: 22, filename: "ch-003" },
     // { name: 'chapter-4', display: 'Chapter 4', length: 23, filename: "ch-004" },
     // { name: 'chapter-5', display: 'Chapter 5', length: 31, filename: "ch-005" }  
   ]  
