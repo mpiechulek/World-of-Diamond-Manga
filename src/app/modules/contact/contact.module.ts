1fdc5d9d755409a1a570e4418781ed73b43ada23
MP <47081376+mpiechulek@users.noreply.github.com>
2021-08-25 08:29:22 +0200

    generated new component

--- src/app/modules/contact/contact.module.ts
+++ src/app/modules/contact/contact.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { ContactRoutingModule } from './contact-routing.module';
 import { ContactContainerComponent } from './page/contact/contact.container';
 import { ContactComponent } from './components/contact/contact.component';
+import { LinksComponent } from './components/contact/links/links.component';
 
 
 @NgModule({
-  declarations: [ContactContainerComponent, ContactComponent],
+  declarations: [ContactContainerComponent, ContactComponent, LinksComponent],
   imports: [
     CommonModule,
     ContactRoutingModule
